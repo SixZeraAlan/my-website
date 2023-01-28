2091693aff94e271278d6fc62e90b30ad090d9ae
Alan <yao.han1@northeastern.edu>
2023-01-28 14:52:39 -0500

    update work tag

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -41,7 +41,7 @@ const Work = () => {
       <h2 className=" head-text">My Creative <span>Portfolio</span> Section</h2>
 
       <div className="app__work-filter">
-        {['UI/UX', 'React JS', , 'React Native App', 'All'].map((item, index) => (
+        {['All'].map((item, index) => (
           <div
             key={index}
             onClick={() => handleWorkFilter(item)}
